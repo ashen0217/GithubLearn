ccadfd7f7857ea87e6c3a5aafd40b8c6468f9951
Ashen Bamunusingha <ashen0217@gmail.com>
2025-06-19 11:56:12 +0530

    as

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ const path ="./data.json"
 const date = moment().subtract(5,'d').format();
 
 
-const data = {
-    date: date,
+
+
+
+const markComits = (n) => {
+    const x = random.default.int(0,54);
+    const y = random.default.int(0,6);
+    const date = moment().subtract(1,"y").add(1,"d").add(x,"w").add(y,"d").format();
+
+    const data = {
+        date: date,
+    };
+
+    jsonfile.writeFile(path, data,()=>{
+        simpleGit().add([path]).commit(date,{'--date':date}).push();
+
+
+    });
 };
 
-jsonfile.writeFile(path, data,()=>{
-    simpleGit().add([path]).commit(date,{'--date':date}).push();
-
-
-});
 
